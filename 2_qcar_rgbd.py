b4f064eadb19d0d7e4ecdf15fbfa2c3a7ab470cf
Prakash Kannaiah <105844116+prakash-kannaiah@users.noreply.github.com>
2026-03-26 15:36:47 +0530

    Add files via upload

---
+++ 2_qcar_rgbd.py
@@ -0,0 +1,27 @@
+﻿import cv2
+from pal.products.qcar import QCarRealSense, IS_PHYSICAL_QCAR
+
+myCam = QCarRealSense(mode='RGB, Depth')
+
+try:
+    while True:
+        
+        myCam.read_RGB() # Read RGB
+        cv2.imshow('My RGB', myCam.imageBufferRGB)
+
+        myCam.read_depth() # Read depth
+        depth_img = myCam.imageBufferDepthPX
+        cv2.imshow('My Depth', depth_img)
+        shape_depth = depth_img.shape
+
+        # Needed for OpenCV window updates
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            print("\nProgram stopped by user (pressed Q).")
+            break
+
+except KeyboardInterrupt:
+    print("\nProgram stopped by user (CTRL+C).")
+
+print(shape_depth)
+
+cv2.destroyAllWindows()
